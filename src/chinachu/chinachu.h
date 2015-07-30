aae74f26e13e19265800d45716fa9bb03e4a8a7d
Yuki MIZUNO <mizuyuu0904@gmail.com>
2015-07-30 21:06:25 +0900

    :new: Create common utility header file

---
+++ src/chinachu/chinachu.h
@@ -0,0 +1,28 @@
+/*
+ *         Copylight (C) 2015 Yuki MIZUNO
+ *         https://github.com/mzyy94/pvr.chinachu/
+ *
+ *
+ * This file is part of pvr.chinachu.
+ *
+ * pvr.chinachu is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * pvr.chinachu is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with pvr.chinachu.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#ifndef CHINACHU_CHINACHU_H
+#define CHINACHU_CHINACHU_H
+
+#include "schedule.h"
+#include "api.h"
+
+#endif /* end of include guard */
