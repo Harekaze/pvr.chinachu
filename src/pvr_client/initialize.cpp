188ebc571b6a7a90a69fdd18cd0c61715c43df5c
Yuki MIZUNO <mizuyuu0904@gmail.com>
2015-11-04 21:48:21 +0900

    :fire: Delete settings, merge it into initialize

--- src/pvr_client/initialize.cpp
+++ src/pvr_client/initialize.cpp
@@ -154,6 +154,21 @@ void ADDON_Destroy(void) {
 void ADDON_Stop(void) {
 }
 
+// Settings configuration
+
+bool ADDON_HasSettings() {
+	return true;
+}
+
+unsigned int ADDON_GetSettings(ADDON_StructSetting ***sSet) {
+	return 0;
+}
+
+ADDON_STATUS ADDON_SetSetting(const char *settingName, const void *settingValue) {
+	return ADDON_STATUS_NEED_RESTART;
+}
+
+void ADDON_FreeSettings(void) {}
 
 /* not implemented */
 void ADDON_Announce(const char *flag, const char *sender, const char *message, const void *data) {}
