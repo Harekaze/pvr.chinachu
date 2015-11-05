7107ba98f6f9e88fbbf53cd19266814f20191204
Yuki MIZUNO <mizuyuu0904@gmail.com>
2015-11-05 15:00:42 +0900

    :up: Bump version to 1.1.0

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
-$version="1.0.1"
+$version="1.1.0"
 & ${env:ProgramFiles(x86)}\MSBuild\14.0\Bin\MSBuild.exe .\VS2015\pvr.chinachu.sln /t:Clean,Build /p:Configuration=Release /p:Platform=x86 /p:DefineConstants="VERSION=\`"$version\`""
 Copy-Item -r .\template\pvr.chinachu .\
 Copy-Item .\ChangeLog.txt .\pvr.chinachu
