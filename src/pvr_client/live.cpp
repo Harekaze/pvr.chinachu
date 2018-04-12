940fee71a87f42dc61c6558168f1b4fb6e55bba2
Yuki MIZUNO <mizuyuu0904@gmail.com>
2018-04-13 02:43:42 +0900

    :tv: Set original title to EPG information

--- src/pvr_client/live.cpp
+++ src/pvr_client/live.cpp
@@ -41,6 +41,7 @@ PVR_ERROR GetEPGForChannel(ADDON_HANDLE handle, const PVR_CHANNEL &channel, time
 
 		tag.iUniqueBroadcastId = epg.iUniqueBroadcastId;
 		tag.strTitle = epg.strTitle.c_str();
+		tag.strOriginalTitle = epg.strOriginalTitle.c_str();
 		tag.iUniqueChannelId = channel.iUniqueId;
 		tag.startTime = epg.startTime;
 		tag.endTime = epg.endTime;
